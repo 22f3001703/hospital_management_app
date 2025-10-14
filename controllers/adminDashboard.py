2fae56a735396176d7af999f0dd996c8b1592b00
Aashish kumar jha <22f3001703@ds.study.iitm.ac.in>
2025-10-14 12:14:35 +0000

    All the registerd doctor is being seen on the admin page

--- controllers/adminDashboard.py
+++ controllers/adminDashboard.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,redirect,request,Response,Blueprint,session
 from database import db
 from models.models import User
+from models.models import Doctor
 
 theadminDashboard = Blueprint('theadminDashboard', __name__)
 @theadminDashboard.route("/dashboard/admin", methods=['GET','POST'])
@@ -8,6 +9,10 @@ def adminDashboard():
     if('user' in session and session['role']=="admin"):
         user=session['user']
         print(user)
-        return render_template("adminDashboard.html",user=user)
+        alldoctors=User.query.filter_by(role="doctor").all()
+        print(alldoctors)
+        doctdetails= Doctor.query.all()
+        print(doctdetails)
+        return render_template("adminDashboard.html",user=user , alldoctors=alldoctors, doctdetails=doctdetails)
     else:
         return redirect("/login")
