4154f34d15733e940794e21f7e9ed1aac591d42e
Aashish kumar jha <22f3001703@ds.study.iitm.ac.in>
2025-10-14 08:15:54 +0000

    Registration of doctor working fine

--- controllers/registerDoctor.py
+++ controllers/registerDoctor.py
@@ -1,8 +1,30 @@
 from flask import Flask,render_template,redirect,request,Response,Blueprint,session
 from database import db
 from models.models import User
+from models.models import Doctor
 
 registertheDoctor = Blueprint('registertheDoctor', __name__)
 @registertheDoctor.route("/dashboard/admin/register-doctor", methods=['GET','POST'])
 def registerDoctor():
+    if('user' in session and session['role']=="admin"):
+        print("Registering the Doctor")
+        if(request.method=="POST"):
+            fullname=request.form.get("fullname")
+            address=request.form.get("address")
+            phone=request.form.get("phone")
+            email=request.form.get("email")
+            username=request.form.get("username")
+            password=request.form.get("password")
+            experience=request.form.get("experience")
+            specialization=request.form.get("specialization")
+
+            new_doctor=Doctor(fullname=fullname,address=address,phone=phone,email=email,username=username,password=password,experience=experience,specialization=specialization)
+            db.session.add(new_doctor)
+            db.session.commit()
+            print("Doctor registered successfully")
+            new_user=User(fullname=fullname,username=username,password=password,role="doctor",status=0)
+            db.session.add(new_user)
+            db.session.commit()
+            print("User as a Doctor registered successfully")
+            return redirect("/dashboard/admin")
     return render_template("registerDoctor.html")
--- models/models.py
+++ models/models.py
@@ -8,3 +8,18 @@ class User(db.Model):
     password = db.Column(db.String(),nullable= False)
     role = db.Column(db.String(10),nullable=False)
     status=db.Column(db.Integer(),nullable=False,default=0)
+    created_at=db.Column(db.DateTime(),default=datetime.datetime.utcnow)
+    updated_at=db.Column(db.DateTime(),default=datetime.datetime.utcnow,onupdate=datetime.datetime.utcnow)
+
+class Doctor(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    fullname = db.Column(db.String(50),nullable = False)
+    username = db.Column(db.String(50),unique = True,nullable = False)
+    password = db.Column(db.String(),nullable= False)
+    address = db.Column(db.String(200),nullable = False)
+    phone = db.Column(db.String(15),nullable = False)
+    email = db.Column(db.String(50),nullable = False)
+    experience = db.Column(db.String(100),nullable = False)
+    specialization = db.Column(db.String(50),nullable = False)
+    created_at=db.Column(db.DateTime(),default=datetime.datetime.utcnow)
+    updated_at=db.Column(db.DateTime(),default=datetime.datetime.utcnow,onupdate=datetime.datetime.utcnow)    
