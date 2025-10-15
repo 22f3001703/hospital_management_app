2357f816739b451e4b50bf34f8d271c74ea1488a
Aashish kumar jha <22f3001703@ds.study.iitm.ac.in>
2025-10-15 10:30:32 +0000

    The department details has been seen to be shown from here

--- controllers/patientDashboard.py
+++ controllers/patientDashboard.py
@@ -1,12 +1,18 @@
 from flask import Flask,render_template,redirect,request,Response,Blueprint,session
 from database import db
 from models.models import User
+from models.models import Department
 
 thepatientDashboard = Blueprint('thepatientDashboard', __name__)
 @thepatientDashboard.route("/dashboard/patient", methods=['GET','POST'])
 def patientDashboard():
     if('user' in session and session['role']=="patient"):
         user=session['user']
-        return render_template("patientDashboard.html",user=user)
+        ##to bring list of departemnts
+        departments= Department.query.all()
+
+        return render_template("patientDashboard.html",user=user, departments=departments)
+    
+
     else:
         return redirect("/login")
