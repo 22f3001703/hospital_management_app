ce499c3b4a7ea83b1d11a8b38b0bd2c15211a360
Aashish kumar jha <22f3001703@ds.study.iitm.ac.in>
2025-10-16 07:26:20 +0000

    changed the nuiances of docavailibility dictionaries

--- controllers/provideAvailibilityToPatient.py
+++ controllers/provideAvailibilityToPatient.py
@@ -1,9 +1,30 @@
 from flask import Flask,render_template,redirect,request,Response,Blueprint,session
 from database import db
-from models.models import User
+from models.models import User,DoctorAvailibility
+import datetime
 
 provideAvailibilityToPatient = Blueprint('provideAvailibilityToPatient', __name__)
 @provideAvailibilityToPatient.route("/dashboard/patient/<string:specialization>/<string:fullname>/<string:username>/availibility", methods=['GET','POST'])
 def provideAvailibilityToThePatient(specialization,fullname,username):
     print(specialization,fullname,username)
-    return render_template("showDoctorAvailibilityToPatient.html")
+    currentdate = datetime.date.today()
+    docavailibility={}
+    next7date=currentdate+datetime.timedelta(days=6)
+    getAvailibility = DoctorAvailibility.query.filter(
+    DoctorAvailibility.username == username,
+    DoctorAvailibility.date>=currentdate,
+    DoctorAvailibility.date<=next7date).order_by(DoctorAvailibility.date).all()
+
+    for lol in getAvailibility:
+        docavailibility[lol.date]=[lol.time1,lol.time2]
+    print(getAvailibility)
+    x=docavailibility.keys()
+    print(x)
+    for al in docavailibility.items():
+        print(al[1][0],al[1][1])
+    docavailibility = {date.isoformat(): slots for date, slots in docavailibility.items()}
+    
+    if(len(x)==0):
+        return render_template("showDoctorAvailibilityToPatient.html",docavailibility=None,x=x)
+    
+    return render_template("showDoctorAvailibilityToPatient.html",docavailibility=docavailibility)
