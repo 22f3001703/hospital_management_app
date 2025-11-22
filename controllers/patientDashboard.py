461a7c2e7fa2b5c9d186cc146a9c1a87197b5ba0
Aashish kumar jha <22f3001703@ds.study.iitm.ac.in>
2025-11-22 15:00:04 +0000

    The doctor details things completely fixed back and availibilty fixed

--- controllers/patientDashboard.py
+++ controllers/patientDashboard.py
@@ -1,18 +1,20 @@
 from flask import Flask,render_template,redirect,request,Response,Blueprint,session
 from database import db
 from models.models import User
-from models.models import Department
+from models.models import Department , Appointments
 
 thepatientDashboard = Blueprint('thepatientDashboard', __name__)
 @thepatientDashboard.route("/dashboard/patient", methods=['GET','POST'])
 def patientDashboard():
     if('user' in session and session['role']=="patient"):
         user=session['user']
-        ##to bring list of departemnts
         departments= Department.query.all()
 
-        return render_template("patientDashboard.html",user=user, departments=departments)
-    
+        myappointmnets= Appointments.query.filter_by(patient=user).all()
+        print("My Appointments:", myappointmnets)
 
+        return render_template("patientDashboard.html",user=user, departments=departments, myappointmnets=myappointmnets)
+    
+    
     else:
         return redirect("/login")
