7f64d95da6ef45ecbe25a6336a1ab33df693835d
Aashish kumar jha <22f3001703@ds.study.iitm.ac.in>
2025-11-24 14:05:58 +0000

    The first step of doing the patient history all way is done, i guees we are done from both side

--- controllers/patientHistory.py
+++ controllers/patientHistory.py
@@ -12,13 +12,38 @@ def patientHistory(pt_username):
             getdoctor= Doctor.query.filter_by(username=user).first()
             department=getdoctor.specialization
             print("Doctor's Department:", department)
-            appointments=Appointments.query.filter_by(patient=pt_username,doctor=user).all()
+            appointments=Appointments.query.filter_by(patient=pt_username).all()
             allrelevantrecordsid=[]
             for x in appointments:
                 if(x.status=="completed"):
                     allrelevantrecordsid.append(x.id)
+            innerrecorddetails={}
+            sendabletreatmentrecords=[]        
             for i in range(len(allrelevantrecordsid)):
                 print("Appointment IDs:", allrelevantrecordsid[i])
+                Treatmentrecord=Treatment.query.filter_by(appointmentid=allrelevantrecordsid[i]).first()
+                print("Treatment Record Fetched:", Treatmentrecord)
+                appointfordoctor = Appointments.query.filter_by(id=allrelevantrecordsid[i]).first()
+                doctorusername=appointfordoctor.doctor
+                doctordetails=Doctor.query.filter_by(username=doctorusername).first()
+                doctorname=doctordetails.fullname
+                department=doctordetails.specialization
+                print("Department for Appointment ID", allrelevantrecordsid[i], "is", department)
+                innerrecorddetails={
+                    "department": department,
+                    "doctorname": doctorname,
+                    "appointment_id": Treatmentrecord.appointmentid,
+                    "diagnosis": Treatmentrecord.diagnosis,
+                    "prescription": Treatmentrecord.prescreption,
+                    "medicines": Treatmentrecord.medicines,
+                    "notes": Treatmentrecord.notes,
+                    "test": Treatmentrecord.test,
+                    "visittype": Treatmentrecord.visittype
+                }
+                print("Inner Record Details:", innerrecorddetails)
+                sendabletreatmentrecords.append(innerrecorddetails)
+            print("All Treatment Records:", sendabletreatmentrecords)
+
             return render_template("patientHistory.html",user=user , appointments=appointments, patient_username=pt_username,department=department, allrelevantrecordsid=allrelevantrecordsid)
     else:
         return redirect("/login")
